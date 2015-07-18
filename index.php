d9fe59b48723938b0d40752328bb9af0f29ba2cf
smitevils <smitevils@yandex.ru>
2015-07-18 21:47:40 +0300

    replace jade to html, unjoin html to other php files

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php include_once("html/main.html") ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<?php include_once("layers/head.php") ?>
+</head>
+<body>
+	<?php include_once("layers/metrica.php") ?>
+	<?php include_once("layers/main.php") ?>
+</body>
+</html>
---
+++ layers/head.php
@@ -0,0 +1,11 @@
+<title>MyRelationship.ru - Мои Родственные Связи - Узнай кто кому кем является!</title>
+<link type="text/css" rel="stylesheet" href="styles/normalize.css">
+<link type="text/css" rel="stylesheet" href="styles/style.css">
+<link href="http://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">
+<script src="scripts/jquery-2.1.4.min.js"></script>
+<script src="scripts/jquery.cookie.js"></script>
+<script src="scripts/script.js"></script>
+<meta name="viewport" content="width=device-width">
+<meta name="keywords" content="кто кому кем является, родственные связи, родство, родня">
+<meta charset="utf-8">
+<link rel="shortcut icon" href="/img/favicon.ico" type="image/ico">
